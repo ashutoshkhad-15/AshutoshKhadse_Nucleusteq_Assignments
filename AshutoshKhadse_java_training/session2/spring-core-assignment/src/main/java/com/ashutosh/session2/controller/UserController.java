e4d4f5192ffd8d3a6cd7d5bf4c4d93231cabca3d
Ashutosh Khadse <ashutoshkhadse2003@gmail.com>
2026-04-10 23:29:47 +0530

    feat: create UserController with GET and POST endpoints

---
+++ AshutoshKhadse_java_training/session2/spring-core-assignment/src/main/java/com/ashutosh/session2/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.ashutosh.session2.controller;
+
+import com.ashutosh.session2.model.User;
+import com.ashutosh.session2.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User created = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+}
