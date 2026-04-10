a52c68b01faaddccecc2be403f650c32363bfa35
Ashutosh Khadse <ashutoshkhadse2003@gmail.com>
2026-04-10 14:40:53 +0530

    chore: initial spring boot project setup inside java/session2

---
+++ AshutoshKhadse_java_training/session2/spring-core-assignment/src/main/java/com/ashutosh/session2/SpringCoreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.session2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreAssignmentApplication.class, args);
+	}
+
+}
---
+++ AshutoshKhadse_java_training/session2/spring-core-assignment/src/test/java/com/ashutosh/session2/SpringCoreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.session2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
