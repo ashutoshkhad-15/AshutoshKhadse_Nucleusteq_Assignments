6abfce422177e360d1182b94e092606bc52a9ce8
Ashutosh Khadse <ashutoshkhadse2003@gmail.com>
2026-04-14 23:03:56 +0530

    chore: initialize Session-3 Spring Boot Maven project with Java 17

---
+++ AshutoshKhadse_java_training/session3/spring-boot-assignment/src/test/java/com/ashutosh/session3/SpringBootAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.session3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
